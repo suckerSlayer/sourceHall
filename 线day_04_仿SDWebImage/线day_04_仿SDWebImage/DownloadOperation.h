ff9d5cc97938d413f82986448fa8d6308d975128
suckerSlayer <978466658@qq.com>
2017-06-23 21:40:06 +0800

    准备DownloadOperation

---
+++ 线day_04_仿SDWebImage/线day_04_仿SDWebImage/DownloadOperation.h
@@ -0,0 +1,13 @@
+//
+//  DownloadOperation.h
+//  线day_04_仿SDWebImage
+//
+//  Created by sean on 17/6/23.
+//  Copyright © 2017年 AAAA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DownloadOperation : NSOperation
+
+@end
---
+++ 线day_04_仿SDWebImage/线day_04_仿SDWebImage/DownloadOperation.m
@@ -0,0 +1,13 @@
+//
+//  DownloadOperation.m
+//  线day_04_仿SDWebImage
+//
+//  Created by sean on 17/6/23.
+//  Copyright © 2017年 AAAA. All rights reserved.
+//
+
+#import "DownloadOperation.h"
+
+@implementation DownloadOperation
+
+@end
