553695b95fe7a4e66d2fa7064abb52dc089aa648
suckerSlayer <978466658@qq.com>
2017-06-23 21:17:33 +0800

    创建和准备文件

---
+++ 线day_04_仿SDWebImage/线day_04_仿SDWebImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  线day_04_仿SDWebImage
+//
+//  Created by sean on 17/6/23.
+//  Copyright © 2017年 AAAA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 线day_04_仿SDWebImage/线day_04_仿SDWebImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  线day_04_仿SDWebImage
+//
+//  Created by sean on 17/6/23.
+//  Copyright © 2017年 AAAA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
